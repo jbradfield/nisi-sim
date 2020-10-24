d9c97980265174b447a6b8170e197d10574b157c
jbradfield <jbradfield@gmail.com>
2020-10-23 23:05:59 -0400

    Updates

---
+++ precache-manifest.94d33dcbbc1bade56ff8e1d6d24ee1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e70095d6e68edd12e57390e880cc544",
+    "url": "/nisi-sim/index.html"
+  },
+  {
+    "revision": "5f80e6a5406e41c91439",
+    "url": "/nisi-sim/static/css/main.e19fd80b.chunk.css"
+  },
+  {
+    "revision": "d2a2ec24ec62e5dc6b13",
+    "url": "/nisi-sim/static/js/2.1532e5b1.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/nisi-sim/static/js/2.1532e5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f80e6a5406e41c91439",
+    "url": "/nisi-sim/static/js/main.57663e8c.chunk.js"
+  },
+  {
+    "revision": "2e609f6c3bf75eb8202c",
+    "url": "/nisi-sim/static/js/runtime-main.2222b064.js"
+  }
+]);
