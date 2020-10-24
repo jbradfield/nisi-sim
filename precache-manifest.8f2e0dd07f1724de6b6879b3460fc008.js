62a3d08ee1c6cfa6e72c9ed035295b1f5c0c608d
jbradfield <jbradfield@gmail.com>
2020-10-24 16:42:49 -0400

    Updates

---
+++ precache-manifest.8f2e0dd07f1724de6b6879b3460fc008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca1a11a264df65950e0a1290334f129",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5b48fb60107d7281fd3",
+    "url": "./static/css/main.3dd6a202.chunk.css"
+  },
+  {
+    "revision": "d2a2ec24ec62e5dc6b13",
+    "url": "./static/js/2.1532e5b1.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.1532e5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b48fb60107d7281fd3",
+    "url": "./static/js/main.372f5e28.chunk.js"
+  },
+  {
+    "revision": "35232443110c0194b1c7",
+    "url": "./static/js/runtime-main.e456e975.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nisi-sim/precache-manifest.bb6bca104bdeb1c859d5c2c395047de0.js"
+  "./precache-manifest.8f2e0dd07f1724de6b6879b3460fc008.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nisi-sim/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
